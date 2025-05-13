f82c64abdc948c5abfa0db822f53ed15ce21b667
nayla110 <naylachindysagita08199@gmail.com>
2025-05-13 21:33:37 +0700

    Profil siswa view dan Controller

---
+++ app/Http/Controllers/ProfilSiswaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfilSiswaController extends Controller
+{
+    public function ProfilSiswa()
+    {
+    // Ini akan mengembalikan tampilan ProfilSiswa.
+    return view('ProfilSiswa');
+    }
+}
