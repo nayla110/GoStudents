6c9657260196300ae493fd50c5d1a3867c801aaf
chia085 <greaciasilalahi0@gmail.com>
2025-05-13 20:53:12 +0700

    Halaman utama siswa view dan Controller

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardStafController;
 use App\Http\Controllers\ProfilStafController;
+use App\Http\Controllers\DashboardSiswaController;
 
 Route::get('/', [LandingPageController::class, 'LandingPage'])->name('LandingPage');
 Route::get('/Login', [LoginController::class, 'Login'])->name('Login');
 Route::get('/DashboardStaf', [DashboardStafController::class, 'DashboardStaf'])->name('DashboardStaf');
 Route::get('/ProfilStaf', [ProfilStafController::class, 'ProfilStaf'])->name('ProfilStaf');
+Route::get('/DashboardSiswa', [DashboardSiswaController::class, 'DashboardSiswa'])->name('DashboardSiswa');
